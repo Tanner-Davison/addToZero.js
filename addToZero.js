d974fdc91d93f15d7b55a7d173b282773be6e488
tannerdavison <tboydavison@gmail.com>
2023-03-23 20:49:06 -0600

    addedInExtraSolution

--- addToZero.js
+++ addToZero.js
@@ -7,8 +7,8 @@ function addTwoZero (addInArray){
     const seenNumbers = {};
     for(let num of addInArray){
         if(-num in seenNumbers){
-        console.log(true);
-        return;
+        return true;
+        
     }else {
         seenNumbers[num]= true;
     }
@@ -16,5 +16,18 @@ function addTwoZero (addInArray){
 console.log(false)
 }
 
+//ORRRRR YOU CAN WRITE IT WITH A NESTED FOR LOOP
+
 console.log(addTwoZero(array))
 
+let answer = false;
+
+for(i=0; i<array.length;i++){
+
+    for(j=i+1; j<array.length;j++){
+        if(array[j]+array[i]===0){
+            answer =true;
+        }
+    }
+}
+console.log(answer)
