323033484b8c40b3ad38c82250bec280f40bb7c7
tannerdavison <tboydavison@gmail.com>
2023-03-25 10:11:30 -0600

    addTwoZeroUsingDoubleforloops

--- addToZero.js
+++ addToZero.js
@@ -3,30 +3,12 @@ let array = [28, 43,-12, 30, 4, 0, 12]
 
 // Write your solution below:
 
-function addTwoZero (addInArray){
-    const seenNumbers = {};
-    for(let num of addInArray){
-        if(-num in seenNumbers){
-        return true;
-        
-    }else {
-        seenNumbers[num]= true;
-    }
-}
-console.log(false)
-}
+let answer= false;
 
-//ORRRRR YOU CAN WRITE IT WITH A NESTED FOR LOOP
-
-console.log(addTwoZero(array))
-
-let answer = false;
-
-for(i=0; i<array.length;i++){
-
-    for(j=i+1; j<array.length;j++){
-        if(array[j]+array[i]===0){
-            answer =true;
+for(let i=0;i<array.length;i++){             //Outer loop
+    for(let j=i+1;j<array.length;j++){
+        if(array[i] + array[j] === 0){
+            answer = true
         }
     }
 }
